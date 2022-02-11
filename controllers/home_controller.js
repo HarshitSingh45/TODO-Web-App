4bd37055598b3ef95b3cdceae60670467851b5c5
harshitsingh45 <harshitsingh45@gmail.com>
2022-02-11 19:06:17 +0530

    views added in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req, res){
-    return res.end('<h1> Express is up for todo app </h1>')
+    // return res.end('<h1> Express is up for todo app </h1>')
+    return res.render('home',{
+        title: 'Reminder app'
+    });
 }
 
 // you can add multiple actions
